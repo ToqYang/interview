af9d9eedaf65d2e013c645c394209e5bf6c8a68a
Santiago Yanguas <santitoya-2001@outlook.com>
2021-11-12 11:26:36 -0500

    [Feature] Routers and components

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import AuthRouter from "./AuthRouter";
+import { useSelector } from "react-redux";
+import HomeScreen from "../components/home/HomeScreen";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+
+const AppRouter = () => {
+  const auth = useSelector((state) => state.auth);
+  const [logged, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (auth?.isAuthenticated === true) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [auth, setIsLoggedIn]);
+
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Switch>
+          <PrivateRoute
+            exact
+            path="/"
+            component={HomeScreen}
+            isAuthenticated={logged}
+          />
+          <PublicRoute
+            path="/auth"
+            component={AuthRouter}
+            isAuthenticated={logged}
+          />
+          <Redirect to="/auth/login" />
+        </Switch>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
+
+export default AppRouter;
