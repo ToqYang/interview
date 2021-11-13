c88a6a4334a4f8ea48ed3b3ca0447e8abdc0e777
Santiago Yanguas <santitoya-2001@outlook.com>
2021-11-13 17:21:05 -0500

    [Feature] Put provider in up components

--- src/InterviewApp.js
+++ src/InterviewApp.js
@@ -1,11 +1,44 @@
+import { useState, useEffect } from "react";
 import AppRouter from "./routers/AppRouter";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { ListContext } from "./store/context";
+import { fetchDataCall } from "./config/handleApi";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [currSelect, setCurrSelect] = useState({
+    _id: 0,
+    done: false,
+    note: "",
+    name: "",
+  });
+  const [restart, setRestart] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetchDataCall();
+      const list = response?.data ?? [];
+      setData(list);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Provider store={store}>
-      <AppRouter />
+      <ListContext.Provider
+        value={{
+          data,
+          setData,
+          currSelect,
+          setCurrSelect,
+          setRestart,
+          restart,
+        }}
+      >
+        <AppRouter />
+      </ListContext.Provider>
     </Provider>
   );
 }
