767d185b95e871527723eb41578fdf3643d7f931
Santiago Yanguas <santitoya-2001@outlook.com>
2021-11-13 17:34:15 -0500

    [Feature] Configuration and helpers

--- src/config/handleApi.js
+++ src/config/handleApi.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { BASE_URL } from "./url_base";
 
-export const fetchDataCall = async ({ api }) => {
+export const fetchDataCall = async () => {
   let apiReturn = await axios
-    .get(api)
+    .get(BASE_URL)
     .then(async function (response) {
       return response;
     })
@@ -10,4 +11,39 @@ export const fetchDataCall = async ({ api }) => {
       console.log(error);
     });
   return apiReturn;
+};
+
+export const ordered = (unordered) => {
+  return Object.keys(unordered)
+    .sort()
+    .reduce((obj, key) => {
+      obj[key] = unordered[key];
+      return obj;
+    }, {});
+};
+
+export const handlePostPut = (mode, data, newList, currSelect, newObj) => {
+  if (mode === "0") {
+    newList = [...data, ordered(newList)];
+  } else if (mode === "1") {
+    newList = data.map((item) => {
+      if (item?._id === currSelect?._id) {
+        return ordered(newObj.original);
+      }
+      return item;
+    });
+  }
+  return newList;
+};
+
+export const handleConfigRequest = (values, mode) => {
+  const newObj = { data: { ...values }, original: { ...values } };
+  delete newObj.data._id;
+  const opt = {
+    url: `${BASE_URL}/${mode === "1" ? `${values._id}` : ""}`,
+    method: mode === "0" ? "POST" : "PUT",
+    data: newObj.data,
+  };
+
+  return { newObj, opt };
 };
--- src/config/url_base.js
+++ src/config/url_base.js
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  "https://crudcrud.com/api/9b6069c69df44079a74c164c51fa40ef";
+  "https://crudcrud.com/api/9739bd7d10b24cd3adb220bb385f8353/todos";
